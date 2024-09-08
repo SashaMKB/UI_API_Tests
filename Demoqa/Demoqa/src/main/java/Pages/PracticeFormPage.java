ec09f3848e48d50fb00362dc4c7019c3adf0c710
SashaMKB <tar-alex@mail.ru>
2024-09-08 22:24:26 +0300

    add two negative tests

--- Demoqa/Demoqa/src/main/java/Pages/PracticeFormPage.java
+++ Demoqa/Demoqa/src/main/java/Pages/PracticeFormPage.java
@@ -9,8 +9,6 @@ import static com.codeborne.selenide.Selenide.$;
 public class PracticeFormPage {
 
     public PracticeFormPage() {
-//        $(By.xpath("//h5[text()='Forms']")).click();
-//        $(By.xpath("//[text()='Forms']")).click();
     }
 
     private static final By firstNameField = By.id("firstName");
@@ -28,32 +26,79 @@ public class PracticeFormPage {
     private static final By successButton = By.xpath("//div[text()='Thanks for submitting the form']");
 
 
-    //возможно, стоит вынести работу с каждым элементом в отдельные методы
-    public void fillAllFields(String firstName, String secondName, String email, String number,
-                              String subject, String address, String state, String city) {
+    public void fillFirstName(String firstName) {
         $(firstNameField).sendKeys(firstName);
-        $(secondNameField).sendKeys(secondName);
-        $(emailField).sendKeys(email);
-        $(numberField).sendKeys(number);
-        $(currentAddressField).sendKeys(address);
-        $(genderMaleField).click();
-        $(hobbiesSportField).click();
+    }
 
+    public void fillSecondName(String secondName) {
+        $(secondNameField).sendKeys(secondName);
+    }
+
+    public void fillEmail(String email) {
+        $(emailField).sendKeys(email);
+    }
+
+    public void fillNumber(String number) {
+        $(numberField).sendKeys(number);
+    }
+
+    public void fillCurrentAddress(String address) {
+        $(currentAddressField).sendKeys(address);
+    }
+
+    public void fillSubject(String subject) {
         $(subjectField).setValue(subject.substring(0, 2));
         $(By.xpath("//*[text()='" + subject + "']")).click();
+    }
 
+    public void fillPhoto() {
         $(pictureField).uploadFile(new File("src/test/resources/1.jpg"));
+    }
 
+    public void fillState(String state) {
         $(stateField).click();
         $(By.xpath("//*[text()='" + state + "']")).click();
+    }
+
+    public void fillCity(String city) {
         $(cityField).click();
         $(By.xpath("//*[text()='" + city + "']")).click();
+    }
+
+    public void fillGenderMaleFiled() {
+        $(genderMaleField).click();
+    }
+
+    public void fillHobbiesSport() {
+        $(hobbiesSportField).click();
+    }
+
+    public void fillAllFields(String firstName, String secondName, String email, String number,
+                              String subject, String address, String state, String city) {
+        fillFirstName(firstName);
+        fillSecondName(secondName);
+        fillEmail(email);
+        fillNumber(number);
+        fillSubject(subject);
+        fillCurrentAddress(address);
+        fillState(state);
+        fillCity(city);
+        fillGenderMaleFiled();
+        fillHobbiesSport();
         $(submitButton).click();
     }
 
 
     public boolean checkThatAllFieldsFillCorrect() {
         return $(successButton).isDisplayed();
+    }
+
+    public boolean checkFieldEmailFillCorrect() {
+        return ($(emailField).getCssValue("border-color")).equals("rgb(206, 212, 218)");
+    }
+
+    public  boolean checkFieldNumberFillCorrect() {
+        return ($(numberField).getCssValue("border-color")).equals("rgb(206, 212, 218)");
     }
 
 
--- Demoqa/Demoqa/src/test/java/PracticeFormTests.java
+++ Demoqa/Demoqa/src/test/java/PracticeFormTests.java
@@ -37,4 +37,34 @@ public class PracticeFormTests {
         page.fillAllFields(firstName, secondName, email, number, subject, address, state, city);
         assertThat(page.checkThatAllFieldsFillCorrect()).isTrue();
     }
+
+    @Test
+    public void fillIncorrectEmailTest() {
+        PracticeFormPage page = new PracticeFormPage();
+        firstName = "name";
+        secondName = "Name";
+        email = "email+email";
+        number = "9177777777";
+        subject = "Maths";
+        address = "address";
+        state = "NCR";
+        city = "Delhi";
+        page.fillAllFields(firstName, secondName, email, number, subject, address, state, city);
+        assertThat(page.checkFieldEmailFillCorrect()).isFalse();
+    }
+
+    @Test
+    public void fillIncorrectNumberTest() {
+        PracticeFormPage page = new PracticeFormPage();
+        firstName = "name";
+        secondName = "Name";
+        email = "emai@example.com";
+        number = "917777772";
+        subject = "Maths";
+        address = "address";
+        state = "NCR";
+        city = "Delhi";
+        page.fillAllFields(firstName, secondName, email, number, subject, address, state, city);
+        assertThat(page.checkFieldNumberFillCorrect()).isFalse();
+    }
 }
